ff51e46172a831dcd45eb594140e512f4fb8fb60
Dan Persons <dogoncouch@users.noreply.github.com>
2017-02-20 23:06:23 -0500

    Metadata update

--- LogDissect/__init__.py
+++ LogDissect/__init__.py
@@ -1,4 +1,4 @@
 __version__ = '0.0.1'
 __author__ = 'Dan Persons'
-__license__ = 'MIT License <http://rfcapture.csail.mit.edu/files/LICENSE.txt>'
+__license__ = 'MIT License'
 __github__ = 'https://github.com/dogoncouch/logdissect'
