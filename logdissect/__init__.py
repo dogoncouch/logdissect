aa5b5f26db7ecae6fa5572857b88161edbf3de06
dogoncouch <dpersonsdev@gmail.com>
2017-03-26 18:11:43 -0400

    Update version: 1.1-pre

--- logdissect/__init__.py
+++ logdissect/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1'
+__version__ = '1.1-pre'
 __author__ = 'Dan Persons <dpersonsdev@gmail.com>'
 __license__ = 'MIT License'
 __github__ = 'https://github.com/dogoncouch/logdissect'
