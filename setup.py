55494cc836dd2ba64efb29c429e5aa123b640cca
dogoncouch <dpersonsdev@gmail.com>
2017-03-28 12:35:53 -0400

    Update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -50,4 +50,4 @@ setup(name = 'logdissect', version = str(__version__),
             "Natural Language :: English",
             "Operating System :: POSIX",
             "Programming Language :: Python :: 2",
-            "Topic :: System :: System Administration"])
+            "Topic :: System :: System Administrations"])
