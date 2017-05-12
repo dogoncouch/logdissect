1b53c006a4523c4cd673ee6f97a8d37e77d276a4
dogoncouch <dpersons617@gmail.com>
2017-05-12 17:23:12 -0400

    Set up logdissect module to import submodules

--- logdissect/__init__.py
+++ logdissect/__init__.py
@@ -4,8 +4,26 @@ __license__ = 'MIT License'
 __github__ = 'https://github.com/dogoncouch/logdissect'
 __all__ = ['core', 'data']
 
-# import logdissect.parsers
-# import logdissect.morphers
-# import logdissect.output
-# import data
-# import core
+import logdissect.parsers
+import logdissect.parsers.syslogbsd
+import logdissect.parsers.syslogiso
+import logdissect.parsers.nohost
+import logdissect.parsers.ldjson
+import logdissect.parsers.tcpdump
+import logdissect.morphers
+import logdissect.morphers.last
+import logdissect.morphers.range
+import logdissect.morphers.grep
+import logdissect.morphers.rgrep
+import logdissect.morphers.source
+import logdissect.morphers.rsource
+import logdissect.morphers.dest
+import logdissect.morphers.rdest
+import logdissect.morphers.process
+import logdissect.morphers.rprocess
+import logdissect.morphers.protocol
+import logdissect.output
+import logdissect.output.log
+import logdissect.output.outjson
+import data
+import core
