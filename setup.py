2bc86cb4bb49cb7e763678ceb1cbdbdb0d21baf5
dogoncouch <dpersonsdev@gmail.com>
2018-03-21 18:29:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,61 +23,84 @@
 Logdissect
 ----------
 
-Logdissect is a command line tool for analyzing syslog files. It can merge entries from multiple log files and sort by timestamp, and filter the results by time range and other criteria. Results are output to the terminal by default, and can also be output to standard syslog file format, or to a JSON array along with some metadata.
+Logdissect is a CLI utility and Python library for analyzing log files and other data. It can parse, merge, filter, and export data (to log files, or JSON).
 
 Options
 ```````
 
 ::
 
-    usage: logdissect [-h] [--dest DEST] [--grep PATTERN] [--last LAST]
-                      [--process PROCESS] [--protocol PROTOCOL] [--range RANGE]
-                      [--rdest RDEST] [--rgrep RPATTERN] [--rprocess RPROCESS]
-                      [--rsource RSOURCE] [--source SOURCE] [--outlog OUTLOG]
-                      [--label LABEL] [--outjson OUTJSON] [--version] [--verbose]
-                      [-s] [--list-parsers] [-p PARSER] [-z] [-t TZONE]
-                      [file [file ...]]
-
+    usage: logdissect.py [-h] [--dhost DHOST] [--grep PATTERN] [--last LAST]
+                         [--process PROCESS] [--protocol PROTOCOL] [--range RANGE]
+                         [--utc] [--rdhost DHOST] [--rgrep PATTERN]
+                         [--rprocess PROCESS] [--rprotocol PROTOCOL]
+                         [--rshost SHOST] [--rsource SOURCE] [--shost SHOST]
+                         [--source SOURCE] [--linejson LINEJSON] [--outlog OUTLOG]
+                         [--label LABEL] [--sojson SOJSON] [--pretty] [--version]
+                         [--verbose] [-s] [--list-parsers] [-p PARSER] [-z]
+                         [-t TZONE]
+                         [file [file ...]]
+    
     positional arguments:
-      file                 specify input files
+      file                  specify input files
     
     optional arguments:
-      -h, --help           show this help message and exit
-      --version            show program's version number and exit
-      --verbose            set verbose terminal output
-      -s                   silence terminal output
-      --list-parsers       return a list of available parsers
-      -p PARSER            select a parser (default: syslogbsd)
-      -z, --unzip          include files compressed with gzip
-      -t TZONE             specify timezone offset to UTC (e.g. '+0500')
+      -h, --help            show this help message and exit
+      --version             show program's version number and exit
+      --verbose             set verbose terminal output
+      -s                    silence terminal output
+      --list-parsers        return a list of available parsers
+      -p PARSER             select a parser (default: syslog)
+      -z, --unzip           include files compressed with gzip
+      -t TZONE              specify timezone offset to UTC (e.g. '+0500')
     
-    morph options:
-      --dest DEST          match a destination host
-      --grep PATTERN       match a pattern
-      --last LAST          match a preceeding time period (e.g. 5m/3h/2d/etc)
-      --process PROCESS    match a source process
-      --protocol PROTOCOL  match a protocol
-      --range RANGE        match a time range (YYYYMMDDhhmm-YYYYMMDDhhmm)
-      --rdest RDEST        filter out a destination host
-      --rgrep RPATTERN     filter out a pattern
-      --rprocess RPROCESS  filter out a source process
-      --rsource RSOURCE    filter out a source host
-      --source SOURCE      match a source host
+    filter options:
+      --dhost DHOST         match a destination host
+      --grep PATTERN        match a pattern
+      --last LAST           match a preceeding time period (e.g. 5m/3h/2d/etc)
+      --process PROCESS     match a source process
+      --protocol PROTOCOL   match a protocol
+      --range RANGE         match a time range (YYYYMMDDhhmm-YYYYMMDDhhmm)
+      --utc                 use UTC for range matching
+      --rdhost DHOST        filter out a destination host
+      --rgrep PATTERN       filter out a pattern
+      --rprocess PROCESS    filter out a source process
+      --rprotocol PROTOCOL  filter out a protocol
+      --rshost SHOST        filter out a source host
+      --rsource SOURCE      filter out a log source
+      --shost SHOST         match a source host
+      --source SOURCE       match a log source
     
     output options:
-      --outlog OUTLOG      set the output file for standard log output
-      --label LABEL        set label type for OUTLOG (fname|fpath)
-      --outjson OUTJSON    set the output file for JSON output
+      --linejson LINEJSON   set the output file for line by line JSON output
+      --outlog OUTLOG       set the output file for standard log output
+      --label LABEL         set label type for OUTLOG (fname|fpath)
+      --sojson SOJSON       set the output file for single object JSON output
+      --pretty              use pretty formatting for sojson output
+
+
+    ==== Available parsing modules: ====
+    
+    ciscoios        : cisco ios parsing module
+    linejson        : logdissect object-per-line JSON parsing module
+    sojson          : logdissect single object JSON parsing module
+    syslog          : syslog (standard timestamp) parsing module
+    syslogiso       : syslog (ISO timestamp) parsing module
+    syslognohost    : syslog (standard timestamp, no host) parsing module
+    tcpdump         : tcpdump terminal output parsing module
+    webaccess       : web access log parsing module
+    windowsrsyslog  : windows rsyslog agent log parsing module
 
 Links
 `````
 
 * `Releases <https://github.com/dogoncouch/logdissect/releases/>`_
 * `Usage <https://github.com/dogoncouch/logdissect/blob/master/README.md>`_
-* `API Usage <https://github.com/dogoncouch/logdissect/blob/master/README-API.md>`_
-* `How To Contribute <https://github.com/dogoncouch/logdissect/blob/master/README-DEV.md>`_
+* `API Usage <https://github.com/dogoncouch/logdissect/blob/master/docs/README-API.md>`_
+* `How To Contribute <https://github.com/dogoncouch/logdissect/blob/master/docs/CONTRIBUTING.md>`_
 * `Changelog <https://github.com/dogoncouch/logdissect/blob/master/CHANGELOG.md>`_
-* `Development source <https://github.com/dogoncouch/logdissect/>`_
+* `GitHub <https://github.com/dogoncouch/logdissect/>`_
+* `Development source <https://github.com/dogoncouch/logdissect/tree/dev>`_
 
 """
 
@@ -88,7 +111,7 @@ from logdissect import __version__
 
 ourdata = [(join(prefix, 'share/man/man1'), ['docs/logdissect.1']),
         (join(prefix, 'share/man/man3'), ['docs/logdissect.3']),
-        (join(prefix, 'share/doc/logdissect'), ['README.md', 'README-API.md',
+        (join(prefix, 'share/doc/logdissect'), ['README.md', 'docs/README-API.md',
             'README-DEV.md', 'LICENSE', 'CHANGELOG.md'])]
 
 setup(name = 'logdissect', version = str(__version__),
@@ -103,7 +126,7 @@ setup(name = 'logdissect', version = str(__version__),
             'log-viewer', 'log-parsing', 'python-library',
             'python-module', 'parser', 'parsing', 'library', 'module'],
         packages = ['logdissect', 'logdissect.parsers',
-            'logdissect.morphers', 'logdissect.output'],
+            'logdissect.filters', 'logdissect.output'],
         entry_points = \
                 { 'console_scripts': [ 'logdissect = logdissect.core:main' ]},
         data_files = ourdata,
