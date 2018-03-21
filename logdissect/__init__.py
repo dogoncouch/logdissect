96c5370e2eb70fa61bf29b5293a3c2446063bab7
dogoncouch <dpersonsdev@gmail.com>
2018-03-20 23:12:53 -0400

    Update version: 2.2.2-dev

--- logdissect/__init__.py
+++ logdissect/__init__.py
@@ -1,8 +1,7 @@
-__version__ = '2.2.1'
+__version__ = '2.2.2-dev'
 __author__ = 'Dan Persons <dpersonsdev@gmail.com>'
 __license__ = 'MIT License'
 __github__ = 'https://github.com/dogoncouch/logdissect'
-__all__ = ['core', 'data']
 
 import logdissect.data
 import logdissect.parsers
